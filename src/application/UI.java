2c2bc384efe887815d6c876e15dc5a01ce563aa7
Vitor Ornelas <ornelas.dev@gmail.com>
2024-01-02 17:03:28 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -17,7 +17,7 @@ public class UI {
     private static void printPiece (ChessPiece piece) {
         if (piece == null) {
             System.out.print("-");
-        } else System.out.println(piece);
+        } else System.out.print(piece);
         System.out.print(" ");
     }
 }
