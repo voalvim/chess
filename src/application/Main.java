eebf4646e925a3282794669d9c29a090d83fb458
Vitor Ornelas <ornelas.dev@gmail.com>
2024-01-02 16:36:37 -0300

    Starting to implement classes Board and Piece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,10 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
-        Position pos = new Position(3, 5);
+        Board board = new Board(8, 8);
     }
 }
